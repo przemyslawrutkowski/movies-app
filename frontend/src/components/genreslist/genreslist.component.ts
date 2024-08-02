1ca0f6fbf814f1493245953ae5ff028005468b27
Przemysław Rutkowski <rutkowski_przemyslaw@o2.pl>
2024-08-02 16:33:46 +0200

    Movies list is updated based on selected genres

--- frontend/src/components/genreslist/genreslist.component.ts
+++ frontend/src/components/genreslist/genreslist.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import GenreI from '../../interfaces/genre';
 import { GenresService } from '../../services/genres.service';
@@ -7,17 +8,28 @@ import { GenresService } from '../../services/genres.service';
 @Component({
   selector: 'app-genreslist',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './genreslist.component.html',
   styleUrl: './genreslist.component.css'
 })
 export class GenresListComponent implements OnInit {
   private genresService: GenresService = inject(GenresService);
   public genres: GenreI[] = [];
+  public selectedGenres: { [key: string]: boolean } = {};
+
+  @Output() genresChange = new EventEmitter<GenreI[]>();
 
   ngOnInit() {
     this.genresService.getGenres().subscribe((genres: GenreI[]) => {
       this.genres = genres.sort((a, b) => a.name.localeCompare(b.name));
+      this.genres.forEach((genre) => {
+        this.selectedGenres[genre._id] = false;
+      })
     });
+  }
+
+  public onGenreChange() {
+    const selectedGenres = this.genres.filter((genre) => this.selectedGenres[genre._id]);
+    this.genresChange.emit(selectedGenres);
   }
 }
--- frontend/src/components/movieslist/movieslist.component.ts
+++ frontend/src/components/movieslist/movieslist.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { MoviesService } from '../../services/movies.service';
 import MovieI from '../../interfaces/movie';
+import GenreI from '../../interfaces/genre';
 import { GenresListComponent } from '../genreslist/genreslist.component';
 import { MovieCardComponent } from '../moviecard/moviecard.component';
 import { SearchBarComponent } from '../searchbar/searchbar.component';
@@ -16,11 +17,25 @@ import { SearchBarComponent } from '../searchbar/searchbar.component';
 })
 export class MoviesListComponent implements OnInit {
   private moviesService: MoviesService = inject(MoviesService);
-  public movies: MovieI[] = [];
+  private movies: MovieI[] = [];
+  public filteredMovies: MovieI[] = [];
 
   ngOnInit() {
     this.moviesService.getMovies().subscribe((movies: MovieI[]) => {
       this.movies = movies;
+      this.filteredMovies = movies;
     });
+  }
+
+  public filterMovies(selectedGenres: GenreI[]) {
+    if (selectedGenres.length === 0) {
+      this.filteredMovies = this.movies;
+    } else {
+      this.filteredMovies = this.movies.filter((movie) => {
+        return movie.genres.some((genreId) => {
+          return selectedGenres.some((selectedGenre) => selectedGenre._id === genreId);
+        })
+      })
+    }
   }
 }
